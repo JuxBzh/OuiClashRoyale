2066123c737c62f969fc5198f2976b6dc77035b6
JuxBzh <julien.pedron@gmail.com>
2017-06-21 00:01:08 +0200

    OuiClash Royale
    Update the UI when the HTTP request finishes

--- app/src/main/java/com/jux/ouiclashroyale/ui/HomeActivity.kt
+++ app/src/main/java/com/jux/ouiclashroyale/ui/HomeActivity.kt
@@ -1,9 +1,12 @@
 package com.jux.ouiclashroyale.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
 import com.jux.ouiclashroyale.R
 import com.jux.ouiclashroyale.model.Arena
 import com.jux.ouiclashroyale.network.HttpClient
@@ -27,24 +30,49 @@ class HomeActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
         onRefresh()
     }
 
+    // SwipeRefreshLayout.OnRefreshListener
+    override fun onRefresh() {
+        swipe_refresh_layout.isRefreshing = true
+
+        // Download arenas
+        val request: Request = Request.Builder().url(arenaUrl).build()
+        HttpClient.asynchronousCall(request, object : HttpClientCallback<Array<Arena>> {
+            override fun onSuccess(result: Array<Arena>) {
+                Log.i(tag, "Number of arenas: " + result.size)
+
+                Handler(Looper.getMainLooper()).post({
+                    refreshSucceeded(result)
+                })
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                Handler(Looper.getMainLooper()).post({
+                    refreshFailed()
+                })
+            }
+        }, Array<Arena>::class.java)
+    }
+
     private fun setupSwipeRefreshLayout() {
         swipe_refresh_layout.setOnRefreshListener(this)
         swipe_refresh_layout.setColorSchemeResources(R.color.colorPrimary)
     }
 
-    // SwipeRefreshLayout.OnRefreshListener
-    override fun onRefresh() {
-        swipe_refresh_layout.isRefreshing = true
+    private fun refreshSucceeded(arenas: Array<Arena>) {
+        // Hide refresh indicator
+        swipe_refresh_layout.isRefreshing = false
 
-        // Perform API call
-        val request: Request = Request.Builder().url(arenaUrl).build()
-        HttpClient.asynchronousCall(request, object : HttpClientCallback<Array<Arena>> {
-            override fun onSuccess(result: Array<Arena>) {
-                Log.i(tag, "Number of arenas: " + result.size)
-            }
+        // Make the list of arenas visible
+        empty.visibility = View.GONE
+        list.visibility = View.VISIBLE
+    }
 
-            override fun onFailure(call: Call?, e: IOException?) {
-            }
-        }, Array<Arena>::class.java)
+    private fun refreshFailed() {
+        // Hide refresh indicator
+        swipe_refresh_layout.isRefreshing = false
+
+        // Make "empty" view visible
+        empty.visibility = View.VISIBLE
+        list.visibility = View.GONE
     }
 }
