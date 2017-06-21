92b374960dd8a21284b159ffad46d5e15d7190dc
JuxBzh <julien.pedron@gmail.com>
2017-06-22 00:02:57 +0200

    OuiClashRoyale
    Using Glide to download the logo of each arena

--- app/src/main/java/com/jux/ouiclashroyale/ui/home/ArenaAdapter.kt
+++ app/src/main/java/com/jux/ouiclashroyale/ui/home/ArenaAdapter.kt
@@ -3,6 +3,8 @@ package com.jux.ouiclashroyale.ui.home
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
 import com.jux.ouiclashroyale.R
 import com.jux.ouiclashroyale.model.Arena
 import com.jux.recyclerviewtoolkit.adapter.BaseAdapter
@@ -10,6 +12,7 @@ import com.jux.recyclerviewtoolkit.viewholder.BaseViewHolder
 
 
 class ArenaAdapter(context: Context) : BaseAdapter<Arena>(context) {
+    val imageLoader: RequestManager by lazy { Glide.with(mContext) }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BaseViewHolder {
         val itemView = LayoutInflater.from(mContext).inflate(R.layout.list_item_arena, parent, false)
@@ -22,6 +25,12 @@ class ArenaAdapter(context: Context) : BaseAdapter<Arena>(context) {
 
         viewHolder.name.text = arena.name
         viewHolder.trophies.text = mContext.getString(R.string.min_trophies, arena.minTrophies)
+
+        imageLoader
+                .load("http://www.clashapi.xyz/images/arenas/${arena.idName}.png")
+                .asBitmap()
+                .centerCrop()
+                .into(viewHolder.logo)
     }
 
     override fun getItemId(position: Int): Long {
